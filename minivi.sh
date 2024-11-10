661e425fd4312491816c464679a44ff601ea87f2
Aleksandr Shamaraev <153871261+AlexanderShad@users.noreply.github.com>
2024-11-10 20:03:30 +1100

    update sh for Fedora

--- minivi.sh
+++ minivi.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Installation script, requires sudo rights
 wget -c https://github.com/AlexanderShad/minivi/releases/download/0.9.0/minivi.tar.xz
-tar -xvf minivi.tar.xz -C $TMP/
+sudo tar -xvf minivi.tar.xz -C $TMP/
 rm minivi.tar.xz
 sudo install -Dm755 $TMP/minivi /usr/bin/minivi
-rm $TMP/minivi
+sudo rm $TMP/minivi
 sudo mv -f $TMP/minivi.png /usr/share/icons/hicolor/128x128/apps/minivi.png
 sudo mv -f $TMP/minivi.desktop /usr/share/applications/minivi.desktop
 echo ''
